032b1a0477b61a6b79f2ebd4c0369f8fea4fc80d
Oliver Klemenz <oliver.klemenz@sap.com>
2020-05-13 21:42:52 +0200

    Update package.lock Update CDS server

--- srv/server.js
+++ srv/server.js
@@ -3,6 +3,20 @@
 const cds = require("@sap/cds");
 const proxy = require("../lib");
 
-cds.on("bootstrap", (app) => app.use(proxy()));
+const init = require("../test/_env/data/init");
+
+let app;
+
+cds.on("bootstrap", (_app) => {
+  app = _app;
+  app.use(proxy())
+});
+
+cds.on("served", (services) => {
+});
+
+cds.on("listening", (server) => {
+  init({ app })
+});
 
 module.exports = cds.server;
