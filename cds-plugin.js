b79cd3ff8ce1b6245fcbe4310123d257c2abd8c8
Oliver Klemenz <oliver.klemenz@sap.com>
2023-11-10 11:43:08 +0100

    Failsafe plugin detection

--- cds-plugin.js
+++ cds-plugin.js
@@ -8,13 +8,19 @@ const cov2ap = require("./src");
 const PLUGIN = "@cap-js-community/odata-v2-adapter";
 const PLUGIN_CONFIG = [PLUGIN, `${PLUGIN}/cds-plugin`, `${PLUGIN}/cds-plugin.js`];
 
-const packageJSON = require(path.join(cds.root, "package.json"));
-const pluginActive =
-  (cds.env.cov2ap && cds.env.cov2ap.plugin) ||
-  (packageJSON &&
-    packageJSON.cds &&
-    packageJSON.cds.plugins &&
-    !!packageJSON.cds.plugins.find((plugin) => PLUGIN_CONFIG.includes(plugin) || PLUGIN_CONFIG.includes(plugin.impl)));
+let pluginActive = cds.env.cov2ap && cds.env.cov2ap.plugin;
+if (!pluginActive) {
+  try {
+    const packageJSON = require(path.join(cds.root, "package.json"));
+    pluginActive =
+      packageJSON &&
+      packageJSON.cds &&
+      packageJSON.cds.plugins &&
+      !!packageJSON.cds.plugins.find((plugin) => PLUGIN_CONFIG.includes(plugin) || PLUGIN_CONFIG.includes(plugin.impl));
+  } catch (e) {
+    // ignore
+  }
+}
 
 if (pluginActive) {
   cds.on("bootstrap", async (app) => {
