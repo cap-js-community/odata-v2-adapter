9e1bff8ed8565b52caf92eedfef3a5fab5d4dba1
Oliver Klemenz <oliver.klemenz@sap.com>
2023-11-30 16:57:10 +0100

    Revise build plugin

--- src/build.js
+++ src/build.js
@@ -9,25 +9,37 @@ module.exports = class COV2APBuildPlugin extends cds.build.BuildPlugin {
     return cds.env.cov2ap && cds.env.cov2ap.plugin;
   }
 
-  async build() {
-    const sidecarEnv = cds.env.for("cds", this.task.src);
+  init() {
+    const mtxBuildTask = this.context.tasks.find(task => task.for === "mtx-sidecar");
+    this.mtxSidecar = !!mtxBuildTask;
+    if (!this.mtxSidecar) {
+      return;
+    }
+    this.task.src = mtxBuildTask.src;
+    const sidecarEnv = cds.env.for("cds", mtxBuildTask.src);
     const modelProviderService = sidecarEnv.requires["cds.xt.ModelProviderService"];
-    const main = (modelProviderService && modelProviderService.root) || "_main"; // TODO: why root not _main
-    const destRoot = path.join(this.task.dest, "mtx/sidecar"); // TODO: how to get dest already on mtx/sidecar
-    const destMain = path.join(destRoot, main);
-    const destMainSrv = path.join(destMain, cds.env.folders.srv);
-    const model = await this.model();
+    this.task.dest = path.join(mtxBuildTask.dest, modelProviderService.root, cds.env.folders.srv, "odata/v2");
+  }
+
+  async build() {
+    if (!this.mtxSidecar) {
+      return;
+    }
+    const model = await this.model(); // TODO: Use this.baseModel(), when available
     if (!model) {
       return;
     }
     const services = cds.reflect(model).services.filter((service) => this.isServedViaOData(service));
     for (const service of services) {
+      if (model.definitions && model.definitions[service.name] && model.definitions[service.name]["@cov2ap.ignore"]) {
+        continue;
+      }
       try {
         const result = await cds.compile.to.edmx(model, {
           service: service.name,
           version: "v2",
         });
-        this.write(result).to(path.join(destMainSrv, "odata/v2", `${service.name}.xml`));
+        this.write(result).to(`${service.name}.xml`);
       } catch (err) {
         this._logger.info(
           `EDMX V2 compilation failed. Service '${service.name}' is (probably) not compatible with OData V2`,
