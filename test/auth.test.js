aa6104d4d143cb2c05e098ee5a50183f86464af0
Oliver Klemenz <oliver.klemenz@sap.com>
2024-05-08 08:04:50 +0200

    Do not log authorization header in debug traces

--- test/auth.test.js
+++ test/auth.test.js
@@ -5,6 +5,11 @@ const supertest = require("supertest");
 
 const util = require("./_env/util/request");
 
+process.env.CDS_LOG_LEVELS_COV2AP = "debug";
+const consoleDebugSpy = jest.spyOn(global.console, "debug");
+consoleDebugSpy.mockImplementation(() => {
+});
+
 cds.test(__dirname + "/_env");
 
 let request;
@@ -25,27 +30,27 @@ describe("auth", () => {
 
   it("GET $metadata auth", async () => {
     let response = await util.callRead(request, "/odata/v2/auth/$metadata", {
-      accept: "application/xml",
+      accept: "application/xml"
     });
     expect(response.status).toEqual(401);
-    expect(response.headers["www-authenticate"]).toEqual('Basic realm="Users"');
+    expect(response.headers["www-authenticate"]).toEqual("Basic realm=\"Users\"");
 
     response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
-      Authorization: invalidAuth,
+      Authorization: invalidAuth
     });
     expect(response.status).toEqual(403);
 
     response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
-      Authorization: validAuth,
+      Authorization: validAuth
     });
     expect(response.status).toEqual(200);
 
     response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     });
     expect(response.status).toEqual(401);
   });
@@ -54,7 +59,7 @@ describe("auth", () => {
     const consoleSpy = jest.spyOn(console, "error");
     let response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
-      Authorization: "Bearer xyz",
+      Authorization: "Bearer xyz"
     });
     expect(response.status).toEqual(401);
     expect(consoleSpy).toHaveBeenCalledWith(
@@ -64,7 +69,7 @@ describe("auth", () => {
     );
     response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
-      Authorization: validAuth,
+      Authorization: validAuth
     });
     expect(response.status).toEqual(200);
   });
@@ -73,7 +78,7 @@ describe("auth", () => {
     const consoleSpy = jest.spyOn(console, "error");
     let response = await util.callRead(request, "/odata/v2/auth/", {
       accept: "application/xml",
-      Authorization: "Bearer xyz",
+      Authorization: "Bearer xyz"
     });
     expect(response.status).toEqual(401);
     expect(consoleSpy).toHaveBeenCalledWith(
@@ -83,7 +88,7 @@ describe("auth", () => {
     );
     response = await util.callRead(request, "/odata/v2/auth/", {
       accept: "application/xml",
-      Authorization: validAuth,
+      Authorization: validAuth
     });
     expect(response.status).toEqual(200);
   });
@@ -91,11 +96,30 @@ describe("auth", () => {
   it("GET $metadata check response correlation", async () => {
     const response = await util.callRead(request, "/odata/v2/auth/$metadata", {
       accept: "application/xml",
-      Authorization: validAuth,
+      Authorization: validAuth
     });
     expect(response.status).toEqual(200);
     expect(response.headers["x-request-id"]).toBeDefined();
     expect(response.headers["x-correlation-id"]).toBeDefined();
     expect(response.headers["x-correlationid"]).toBeDefined();
+  });
+
+  it("GET sanitize authorization header for debug traces", async () => {
+    consoleDebugSpy.mockReset();
+    const response = await util.callRead(request, "/odata/v2/auth/Header", {
+      accept: "application/xml",
+      Authorization: validAuth
+    });
+    expect(response.status).toEqual(200);
+    const traceRequest = consoleDebugSpy.mock.calls.find(call => call[1] === "Request:");
+    expect(traceRequest).toBeDefined();
+    expect(traceRequest[2]).toMatch(/Basic \*\*\*/)
+    const traceProxyRequest = consoleDebugSpy.mock.calls.find(call => call[1] === "ProxyRequest:");
+    expect(traceProxyRequest).toBeDefined();
+    expect(traceProxyRequest[2]).toMatch(/Basic \*\*\*/)
+    const traceResponse = consoleDebugSpy.mock.calls.find(call => call[1] === "Response:");
+    expect(traceResponse).toBeDefined();
+    const traceProxyResponse = consoleDebugSpy.mock.calls.find(call => call[1] === "ProxyResponse:");
+    expect(traceProxyResponse).toBeDefined();
   });
 });
